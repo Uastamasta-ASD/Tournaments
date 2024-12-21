585ef2f6dc05711ee4a6249544106636424bf58e
fren_gor <goro@frengor.com>
2024-12-21 23:15:31 +0100

    Define duel additional data

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 #![forbid(unsafe_code)]
 
+use serde::{Deserialize, Serialize};
+
 pub mod traits;
 pub mod groups;
 
 /// Minimum number of players in a tournament.
 pub const MIN_PLAYER_NUMBER: usize = groups::MIN_PLAYERS_PER_GROUP;
+
+#[derive(Serialize, Deserialize, Default, PartialEq, Eq, PartialOrd, Ord, Debug)]
+#[non_exhaustive]
+pub struct AdditionalData {
+}
