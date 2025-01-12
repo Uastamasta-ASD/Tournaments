dd88cab93b245f6664d395ea2a7a8716c517fe13
fren_gor <goro@frengor.com>
2025-01-12 05:07:12 +0100

    Consider `play_offs::MIN_TEAMS` in `MIN_TEAMS_TOURNAMENT`

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,13 @@ pub mod standings;
 pub mod play_offs;
 
 /// Minimum number of teams in a tournament.
-pub const MIN_TEAMS_TOURNAMENT: usize = groups::MIN_TEAMS_PER_GROUP;
+pub const MIN_TEAMS_TOURNAMENT: usize = max(groups::MIN_TEAMS_PER_GROUP, play_offs::MIN_TEAMS);
+
+// The stdlib one isn't const
+const fn max(n1: usize, n2: usize) -> usize {
+    if n1 < n2 {
+        n1
+    } else {
+        n2
+    }
+}
